f9a9dd29bcd9d0aa10674c487921828053c538b9
actingvictor <60287283+actingvictor@users.noreply.github.com>
2021-01-31 14:52:20 +1300

    Update new_demo.py

--- Python Code/new_demo.py
+++ Python Code/new_demo.py
@@ -8,3 +8,32 @@ x = 5
 y = "Hello, World"
 
 print(x, y)
+
+
+# Python Variables
+# Creating Variables
+
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+
+x = 4   # x is of type into
+y = "sally"  # x is now of type str
+print(x)
+print(y)
+
+# Casting
+
+x = str(3)      # x will be '3'
+y = int(3)      # y will be 3
+z = float(3)    # z will be 3.0
+
+# Variables are case sensitive
+
+a = 4
+A = "Sally"
+print(a)
+print(A)
